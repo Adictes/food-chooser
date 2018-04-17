9c0af0a50698ae4deeceee336e99c59795b49ac0
SimePel <simepelmain@gmail.com>
2018-04-17 18:33:03 +0700

    Improve output info. Used prettier

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,28 @@
 var frws = new WebSocket("wss://" + window.location.host + "/frws");
 
-frws.onmessage = function (event) {
-    var data = JSON.parse(event.data);
-    console.log(data);
+var markers = [];
+frws.onmessage = function(event) {
+    deletePostsFromList();
+    deleteMarkersFromMap();
 
+    var data = JSON.parse(event.data);
     for (const i in data) {
         var marker = new google.maps.Marker({
             position: data[i].geometry.location,
             map: map,
         });
+        markers.push(marker);
+
         var node = document.createElement("li");
         node.classList.add("list-group-item");
-        var text = document.createTextNode(data[i].name);
-        node.appendChild(text);
+        node.innerHTML = data[i].name + " Адрес: " + data[i].formatted_address;
         document.getElementById("info-list").appendChild(node);
     }
 };
 
 function sendRequest() {
     navigator.geolocation.getCurrentPosition(
-        function (position) {
+        function(position) {
             var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude,
@@ -35,23 +38,29 @@ function sendRequest() {
                 query;
             frws.send(str);
         },
-        function (error) {
+        function(error) {
             var infoWindow = new google.maps.InfoWindow({
                 map: map,
                 position: map.getCenter(),
             });
             switch (error.code) {
                 case error.PERMISSION_DENIED:
-                    infoWindow.setContent("User denied the request for Geolocation.");
+                    infoWindow.setContent(
+                        "User denied the request for Geolocation."
+                    );
                     break;
                 case error.POSITION_UNAVAILABLE:
-                    infoWindow.setContent("Location information is unavailable.")
+                    infoWindow.setContent(
+                        "Location information is unavailable."
+                    );
                     break;
                 case error.TIMEOUT:
-                    infoWindow.setContent("The request to get user location timed out.")
+                    infoWindow.setContent(
+                        "The request to get user location timed out."
+                    );
                     break;
                 case error.UNKNOWN_ERROR:
-                    infoWindow.setContent("An unknown error occurred.")
+                    infoWindow.setContent("An unknown error occurred.");
                     break;
             }
             frws.send("error");
@@ -65,6 +74,21 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 53.3548, lng: 83.7698 },
-        zoom: 16,
+        zoom: 15,
     });
+}
+
+// Clear previous responses from list
+function deletePostsFromList() {
+    var list = document.getElementById("info-list");
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function deleteMarkersFromMap() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
 }
